51dc2170a67071f904486295115b959dd99c5ddd
ehsana00 <a12346516@unet.univie.ac.at>
2025-12-04 22:49:16 +0100

    Login Screen

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:chatapp/core/common/custom_button.dart';
 import 'package:chatapp/core/common/custom_text_field.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -15,37 +17,64 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Form(
-          child: Column(
-            children: [
-              SizedBox(height: 30),
-              Text(
-                "Welcome Back",
-                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 30),
+                Text(
+                  "Welcome Back",
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Sign In to continue",
-                style: Theme.of(
-                  context,
-                ).textTheme.bodyLarge?.copyWith(color: Colors.grey),
-              ),
-              SizedBox(),
-              CustomTextField(
-                controller: emailController,
-                hintText: "Email",
-                prefixIcon: Icon(Icons.email),
-              ),
-              CustomTextField(
-                controller: passwordController,
-                hintText: "Password",
-                obscureText: true,
-                prefixIcon: Icon(Icons.password),
-                suffixIcon: Icon(Icons.visibility),
-              ),
-            ],
+                SizedBox(height: 10),
+                Text(
+                  "Sign In to continue",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyLarge?.copyWith(color: Colors.grey),
+                ),
+                SizedBox(height: 30),
+                CustomTextField(
+                  controller: emailController,
+                  hintText: "Email",
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+                SizedBox(height: 16),
+                CustomTextField(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                  prefixIcon: Icon(Icons.password_outlined),
+                  suffixIcon: Icon(Icons.visibility),
+                ),
+                SizedBox(height: 30),
+                CustomButton(onPressed: () {}, text: "Login"),
+                SizedBox(height: 16),
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Don't have an account?  ",
+                      style: TextStyle(color: Colors.grey[600]),
+                      children: [
+                        TextSpan(
+                          text: "Sign Up",
+                          style: Theme.of(context).textTheme.bodyLarge
+                              ?.copyWith(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                          recognizer: TapGestureRecognizer(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
